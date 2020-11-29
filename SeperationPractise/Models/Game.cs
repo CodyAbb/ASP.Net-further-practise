b6318ef55a9a03bdaf2edec20cde1ffa50d567d2
Cody Abbott <cody.abbott16@gmail.com>
2020-11-29 20:13:36 +0000

    Added models for game and publisher

---
+++ SeperationPractise/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SeperationPractise.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AgeRating { get; set; }
+        public int YearReleased { get; set; }
+
+        public Publisher Publisher { get; set; }
+    }
+}
---
+++ SeperationPractise/Models/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeperationPractise.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double NetWorth { get; set; }
+        public ICollection<Game> PublishedGames { get; set; }
+    }
+}
